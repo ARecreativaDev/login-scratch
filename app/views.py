6fadb0f4513618c17e232fb1900043cc9b737532
Caio Henrique <caio.madalena@alunos.ifsuldeminas.edu.br>
2023-01-26 11:12:10 -0300

    make it better

--- app/views.py
+++ app/views.py
@@ -8,9 +8,16 @@ from webdriver_manager.chrome import ChromeDriver
 from selenium.webdriver.chrome.service import Service
 import pandas as pd
 import time
+import os
 from django.views.decorators.csrf import requires_csrf_token
 # def (request):
 #    return (request, '')
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
 
 
 dado = "https://www.arecreativa.com.br/scratchLogins.json"
@@ -30,7 +37,7 @@ def dologin(request):
         if row["login"] == str(user):
 
             
-            navegador = webdriver.Chrome(executable_path='/path/to/chromedriver')
+            navegador = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
             navegador.get("https://scratch.mit.edu/accounts/login/")
             time.sleep(3)
             
